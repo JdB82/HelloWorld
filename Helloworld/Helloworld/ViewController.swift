6839e0567e278be75af91d84628a7e7666ec4ded
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-18 12:29:40 +0100

    New label work.

--- Helloworld/Helloworld/ViewController.swift
+++ Helloworld/Helloworld/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var ouputLabel1: UILabel!
+    
+    @IBOutlet weak var weatherButtonOulet: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func doSomething(_ sender: AnyObject) {
+        
+        let box1 = "RUN FOREST RUN"
+        outputLabel.text = box1
+    }
+    
+    @IBAction func tellWeTheWeather(_ sender: Any) {
+    
+        var message = ""
+        let weather = "Sunny"
+        
+        var rain:Bool = false
+        var sun5:Bool = false
+    
+        
+        if rain {
+            message = "raining"
+            print("raining")
+        } else {
+            
+            message = "is not raining"
+            print("clear")
+        }
+        
+        
+        if sun5 {
+            print("sunny")
+        } else {
+            print("tunder")
+        }
+        
+        ouputLabel1.text = message
+        weatherButtonOulet.titleLabel?.text = message
+
+        
+    }
 
 }
 
